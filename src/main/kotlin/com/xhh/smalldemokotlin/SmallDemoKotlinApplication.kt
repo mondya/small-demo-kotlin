dbdc26d275c55de460698842b4d1a615aa89478c
xhh <xhh19990210@gmail.com>
2025-03-16 22:42:32 +0800

    init

---
+++ src/main/kotlin/com/xhh/smalldemokotlin/SmallDemoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.xhh.smalldemokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmallDemoKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmallDemoKotlinApplication>(*args)
+}
