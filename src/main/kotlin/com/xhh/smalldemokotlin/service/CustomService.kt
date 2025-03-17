ce24fe5d5d7b658847f63d3fcef6a689079abfbe
xhh <xhh19990210@gmail.com>
2025-03-17 10:31:44 +0800

    kotlin

--- src/main/kotlin/com/xhh/smalldemokotlin/service/CustomService.kt
+++ src/main/kotlin/com/xhh/smalldemokotlin/service/CustomService.kt
@@ -1,4 +1,12 @@
 package com.xhh.smalldemokotlin.service
 
+import com.xhh.smalldemokotlin.domain.CustomEntity
+import org.springframework.stereotype.Service
+
+@Service
 class CustomService {
+
+    fun findAll(): MutableList<CustomEntity> {
+        return mutableListOf()
+    }
 }
